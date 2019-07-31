207966d7297efd167cf0a5bcd84ccad0d3f12fc3
ramonmacias <ramonmaciasgarcia@gmail.com>
2019-07-31 16:12:20 +0200

    Added all the rest API endpoints

--- internal/app/interface/restApi/router.go
+++ internal/app/interface/restApi/router.go
@@ -10,6 +10,8 @@ func BuildRouter() *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/users", ListAllUsers).Methods("GET")
 	r.HandleFunc("/users", CreateUser).Methods("POST")
+	r.HandleFunc("/users/{id}", RemoveUser).Methods("DELETE")
+	r.HandleFunc("/users/{id}", FindUserByID).Methods("GET")
 
 	http.Handle("/", r)
 	return r

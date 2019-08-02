f04b09602ec5d4a9a0087c4851a1861e26740a12
ramonmacias <ramonmaciasgarcia@gmail.com>
2019-08-02 13:04:41 +0200

    Create a new handler switch for select between memory and postgres persistance access

--- internal/app/interface/persistance/postgres/client.go
+++ internal/app/interface/persistance/postgres/client.go
@@ -35,7 +35,7 @@ func NewClient(host, port, user, dbname, password string) *client {
 }
 
 func (c *client) Connect() *Connection {
-	if connInstance != nil {
+	if connInstance == nil {
 		db, err := gorm.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s", c.host, c.port, c.user, c.dbname, c.password))
 		if err != nil {
 			log.Panicf("Error trying to connect: %v", err)
@@ -45,4 +45,8 @@ func (c *client) Connect() *Connection {
 		}
 	}
 	return connInstance
+}
+
+func (c *Connection) DB() *gorm.DB {
+	return c.conn
 }

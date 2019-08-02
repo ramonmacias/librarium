b545c1bd8c3c173b8fecbb6a4c1d6f48f9cf0c1b
ramonmacias <ramonmaciasgarcia@gmail.com>
2019-08-02 11:07:46 +0200

    Added new management with a new type of persistance

--- internal/app/interface/restApi/router.go
+++ internal/app/interface/restApi/router.go
@@ -8,10 +8,10 @@ import (
 
 func BuildRouter() *mux.Router {
 	r := mux.NewRouter()
-	r.HandleFunc("/users", ListAllUsers).Methods("GET")
-	r.HandleFunc("/users", CreateUser).Methods("POST")
-	r.HandleFunc("/users/{id}", RemoveUser).Methods("DELETE")
-	r.HandleFunc("/users/{id}", FindUserByID).Methods("GET")
+	r.HandleFunc("/{persistance_type}/users", ListAllUsers).Methods("GET")
+	r.HandleFunc("/{persistance_type}/users", CreateUser).Methods("POST")
+	r.HandleFunc("/{persistance_type}/users/{id}", RemoveUser).Methods("DELETE")
+	r.HandleFunc("/{persistance_type}/users/{id}", FindUserByID).Methods("GET")
 
 	http.Handle("/", r)
 	return r

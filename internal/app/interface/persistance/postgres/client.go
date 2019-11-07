8a4c94d30a097affd5918ea801fee06ff4f9116c
Ramon <ramonmaciasgarcia@gmail.com>
2019-11-07 08:02:47 +0100

    Fixed SSL enabled connection

--- internal/app/interface/persistance/postgres/client.go
+++ internal/app/interface/persistance/postgres/client.go
@@ -36,7 +36,7 @@ func NewClient(host, port, user, dbname, password string) *client {
 
 func (c *client) Connect() *Connection {
 	if connInstance == nil {
-		db, err := gorm.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s", c.host, c.port, c.user, c.dbname, c.password))
+		db, err := gorm.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable", c.host, c.port, c.user, c.dbname, c.password))
 		if err != nil {
 			log.Panicf("Error trying to connect: %v", err)
 		}
